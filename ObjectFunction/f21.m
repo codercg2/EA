5dca7c8722526c0ba3fcc6b5b1cd8711cf3b2fc1
chenggang816 <1305753112@qq.com>
2017-03-20 17:22:01 +0800

    Add object function f2*,f3* and test.

--- ObjectFunction/f21.m
+++ ObjectFunction/f21.m
@@ -1,7 +1,6 @@
-function [ output_args ] = Untitled( input_args )
-%UNTITLED 此处显示有关此函数的摘要
+function [ output ] = f21( x )
+%f21 此处显示有关此函数的摘要
 %   此处显示详细说明
-
-
+output = 1 - exp(-sum((x-1/sqrt(3)).^2));
 end
 
